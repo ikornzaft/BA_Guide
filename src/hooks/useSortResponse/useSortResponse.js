bcb7c929fb953edc3e1d1db4d85832ffcc134f2e
Nacho <ikornzaft@gmail.com>
2021-03-27 17:14:29 -0300

    fix: add new rules in useSortResponse.js

--- src/hooks/useSortResponse/useSortResponse.js
+++ src/hooks/useSortResponse/useSortResponse.js
@@ -2,13 +2,14 @@ const useSortResponse = (el) => {
     let placeName, placeAddress, placeDays, placePhone, placeEmail, placeWebsite; 
     let noData = false;
     if (el) {
+        if (el.direccionNormalizada) placeAddress = el.direccionNormalizada;
         el.contenido.forEach(id => {
             if (id.nombreId === "nombre") placeName = id.valor;
             if (id.nombreId === "direccion") placeAddress = id.valor;
-            if (id.nombreId === "telefonos") placePhone = id.valor;
+            if (id.nombreId === "telefonos" || id.nombreId === "telefono") placePhone = id.valor;
             if (id.nombreId === "dias_y_horarios") placeDays = id.valor;
             if (id.nombreId === "email") placeEmail = id.valor;
-            if (id.nombreId === "pagina_web") placeWebsite = id.valor;
+            if (id.nombreId === "pagina_web" || id.nombreId === "web") placeWebsite = id.valor;
         });
         if (!placePhone && !placeDays && !placeEmail && !placeWebsite) noData = true;
     };
